df97e5705a8d9952a41696463121299781ce60c7
MichelGad <michel.gad@outlook.com>
2022-01-04 23:55:59 +0100

    First Scientific

--- calculator.py
+++ calculator.py
@@ -15,13 +15,14 @@ choice = input('Enter your choice(1/2/3): ')
 
 # Simple Calculator
 if choice == '1':
+    print('**************************')
     print('Basic Calculator')
     print('**************************')
     print('Select operation.')
-    print('1.Add')
-    print('2.Subtract')
-    print('3.Multiply')
-    print('4.Divide')
+    print('1.Addition')
+    print('2.Subtraction')
+    print('3.Multiplication')
+    print('4.Division')
     print('**************************')
 
     # check if choice is one of the four options
@@ -58,12 +59,89 @@ if choice == '1':
 # Advanced Calculator
 if choice == '2':
     operator = ''
+    print('**************************')
     print('Advanced Calculator')
+    print('**************************')
+    print('Equation Format: X operator Y')
+    print('**************************')
+    print('Addition +')
+    print('Subtraction -')
+    print('Multiplication *')
+    print('Division /')
     print('**************************')
 
     while True:
 
-        calculation = input("Enter your calculation: ")
+        calculation = input("Enter your equation: ")
+
+        for num in calculation:
+            if num == '+' or num == '-' or num == '*' or num == '/' or num == '^':
+                operator = num.strip()
+                break
+
+        if operator == '+':
+            split_calculation = calculation.split('+', 1)
+            x = float(split_calculation[0])
+            y = float(split_calculation[1])
+            adv = mn.advanced(x, y)
+            result = adv.decision(operator)
+            print(x, operator, y, '=', result)
+
+        if operator == '-':
+            split_calculation = calculation.split('-', 1)
+            x = float(split_calculation[0])
+            y = float(split_calculation[1])
+            adv = mn.advanced(x, y)
+            result = adv.decision(operator)
+            print(x, operator, y, '=', result)
+
+        if operator == '*':
+            split_calculation = calculation.split('*', 1)
+            x = float(split_calculation[0])
+            y = float(split_calculation[1])
+            adv = mn.advanced(x, y)
+            result = adv.decision(operator)
+            print(x, operator, y, '=', result)
+
+        if operator == '/':
+            split_calculation = calculation.split('/', 1)
+            x = float(split_calculation[0])
+            y = float(split_calculation[1])
+            adv = mn.advanced(x, y)
+            result = adv.decision(operator)
+            print(x, operator, y, '=', result)
+
+        # check if user wants another calculation
+        # break the while loop if answer is no
+        quit = input('Do you want to quit the calculator? (y/n): ')
+        if quit == 'y':
+            break
+    else:
+        print("Invalid Input")
+
+# Scientific Calculator
+if choice == '3':
+    operator = ''
+    print('**************************')
+    print('Scientific Calculator')
+    print('**************************')
+    print('Equation Format: X operator Y')
+    print('**************************')
+    print('Addition +')
+    print('Subtraction -')
+    print('Multiplication *')
+    print('Division /')
+    print('Exponent ^')
+    print('tan')
+    print('sin')
+    print('cos')
+    print ('Factorial')
+    print('log')
+    print('**************************')
+
+    while True:
+
+        calculation = input("Enter your equation: ")
 
         for num in calculation:
             if num == '+' or num == '-' or num == '*' or num == '/' or num == '^':
--- main.py
+++ main.py
@@ -40,6 +40,39 @@ class advanced:
     def div(self):
         return self.x / self.y
 
+
+    # Operation Type
+    def decision (self, operator):
+        if operator == '+':
+            return self.add()
+        elif operator == '-':
+            return self.sub()
+        elif operator == '*':
+            return self.multi()
+        elif operator == '/':
+            return self.div()
+
+class scientific:
+    def __init__(self, x, y):
+        self.x= x
+        self.y= y
+
+    # This function adds two numbers
+    def add(self):
+        return self.x + self.y
+
+    # This function subtracts two numbers
+    def sub(self):
+        return self.x - self.y
+
+    # This function multiplies two numbers
+    def multi(self):
+        return self.x * self.y
+
+    # This function divides two numbers
+    def div(self):
+        return self.x / self.y
+
     # This function gives the power of a number
     def power(self):
         return self.x ** self.y
