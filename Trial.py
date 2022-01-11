c4ba3a25a94cff5a504f23fec1d744532add4c79
MichelGad <michel.gad@outlook.com>
2022-01-11 01:37:58 +0100

    Funal

--- Trial.py
+++ Trial.py
@@ -15,91 +15,89 @@ print('Division /       | tan')
 print('Exponent ^       | log')
 print('*********************************')
 
-calculation = input("Enter your equation: ")
-
-for i in calculation:
-    if i == '+' or i == '-' or i == '*' or i == '/' or i == '^' or i == '!':
-        operator = i.strip()
-
-str_operator = ''.join(re.split("[^a-zA-Z]*", calculation))
-num = re.findall(r'-?\d+\.?\d*', calculation)
-
-if str_operator == 'sin' or str_operator == 'cos' or str_operator == 'tan' \
-        or str_operator == 'log' or str_operator == '^':
-    x = float(num[0])
-
 while True:
+    calculation = input("Enter your equation: ")
 
-    if str_operator == 'cos':
-        sci = mn.new_scientific(x)
-        result = sci.str_decision(str_operator)
-        print(str_operator, x, '=', result)
+    for i in calculation:
+        if i == '+' or i == '-' or i == '*' or i == '/' or i == '^' or i == '!':
+            operator = i.strip()
 
-    if str_operator == 'sin':
-        sci = mn.new_scientific(x)
-        result = sci.str_decision(str_operator)
-        print(str_operator, x, '=', result)
+    str_operator = ''.join(re.split("[^a-zA-Z]*", calculation))
+    num = re.findall(r'-?\d+\.?\d*', calculation)
 
-    if str_operator == 'tan':
-        sci = mn.new_scientific(x)
-        result = sci.str_decision(str_operator)
-        print(str_operator, x, '=', result)
+    if str_operator == 'sin' or str_operator == 'cos' or str_operator == 'tan' \
+            or str_operator == 'log' or str_operator == '^':
 
-    if str_operator == 'log':
-        sci = mn.new_scientific(x)
-        result = sci.str_decision(str_operator)
-        print(str_operator, x, '=', result)
+        x = float(num[0])
+        if str_operator == 'cos':
+            sci = mn.new_scientific(x)
+            result = sci.str_decision(str_operator)
+            print(str_operator, x, '=', result)
 
-    quite = input('Do you want to quit this calculator? (y/n): ')
-    if quite == 'y':
-        break
-    elif quite == 'n':
-        continue
-    else:
-        print("Invalid Input")
+        if str_operator == 'sin':
+            sci = mn.new_scientific(x)
+            result = sci.str_decision(str_operator)
+            print(str_operator, x, '=', result)
 
+        if str_operator == 'tan':
+            sci = mn.new_scientific(x)
+            result = sci.str_decision(str_operator)
+            print(str_operator, x, '=', result)
 
-if operator == '+' or operator == '-' or operator == '*' or operator == '/' or operator == '^':
+        if str_operator == 'log':
+            sci = mn.new_scientific(x)
+            result = sci.str_decision(str_operator)
+            print(str_operator, x, '=', result)
 
-    x = float(num[0])
-    y = float(num[1])
-
-    while True:
-        if operator == '+':
-            sci = mn.scientific(x, y)
-            result = sci.decision(operator)
-            print(x, operator, y, '=', result)
-
-        if operator == '-':
-            sci = mn.scientific(x, y)
-            result = sci.decision(operator)
-            print(x, operator, y, '=', result)
-
-        if operator == '*':
-            sci = mn.scientific(x, y)
-            result = sci.decision(operator)
-            print(x, operator, y, '=', result)
-
-        if operator == '/':
-            sci = mn.scientific(x, y)
-            result = sci.decision(operator)
-            print(x, operator, y, '=', result)
-
-        if operator == '^':
-            sci = mn.scientific(x, y)
-            result = sci.decision(operator)
-            print(x, operator, y, '=', result)
-
-        # check if user wants another calculation
-        # break the while loop if answer is no
-        print('If you want to quit: Write(q)')
-        next_step = input('Next Step: ')
-        if next_step == 'q':
+        quite = input('Do you want to quit this calculator? (y/n): ')
+        if quite == 'y':
             break
+        elif quite == 'n':
+            continue
         else:
-            for f in next_step:
-                if f == '+' or f == '-' or f == '*' or f == '/':
-                    operator = f.strip()
-            x = result
-            num = re.findall(r'-?\d+\.?\d*', next_step)
-            y = float(num[0])
+            print("Invalid Input")
+
+    if operator == '+' or operator == '-' or operator == '*' or operator == '/' or operator == '^':
+
+        x = float(num[0])
+        y = float(num[1])
+
+        while True:
+            if operator == '+':
+                sci = mn.scientific(x, y)
+                result = sci.decision(operator)
+                print(x, operator, y, '=', result)
+
+            if operator == '-':
+                sci = mn.scientific(x, y)
+                result = sci.decision(operator)
+                print(x, operator, y, '=', result)
+
+            if operator == '*':
+                sci = mn.scientific(x, y)
+                result = sci.decision(operator)
+                print(x, operator, y, '=', result)
+
+            if operator == '/':
+                sci = mn.scientific(x, y)
+                result = sci.decision(operator)
+                print(x, operator, y, '=', result)
+
+            if operator == '^':
+                sci = mn.scientific(x, y)
+                result = sci.decision(operator)
+                print(x, operator, y, '=', result)
+
+            # check if user wants another calculation
+            # break the while loop if answer is no
+            print('If you want to quit: Write(q)')
+            next_step = input('Next Step: ')
+            if next_step == 'q':
+                break
+            else:
+                for f in next_step:
+                    if f == '+' or f == '-' or f == '*' or f == '/':
+                        operator = f.strip()
+                x = result
+                num = re.findall(r'-?\d+\.?\d*', next_step)
+                y = float(num[0])
--- calculator.py
+++ calculator.py
@@ -154,96 +154,102 @@ while True:
         print('*********************************')
         print('Equation Format: X operator Y')
         print('*********************************')
-        print('Addition +       | Factorial !')
+        print('Addition +       | ')
         print('Subtraction -    | sin')
         print('Multiplication * | cos')
         print('Division /       | tan')
         print('Exponent ^       | log')
         print('*********************************')
 
-        calculation = input("Enter your equation: ")
-
-        for i in calculation:
-            if i == '+' or i == '-' or i == '*' or i == '/' or i == '^' or i == '!':
-                operator = i.strip()
-
-        str_operator = ''.join(re.split("[^a-zA-Z]*", calculation))
-
-        num = re.findall(r'-?\d+\.?\d*', calculation)
-        x = float(num[0])
-        y = float(num[1])
-
         while True:
-            if operator == '+':
-                sci = mn.scientific(x, y)
-                result = sci.decision(operator)
-                print(x, operator, y, '=', result)
-
-            if operator == '-':
-                sci = mn.scientific(x, y)
-                result = sci.decision(operator)
-                print(x, operator, y, '=', result)
-
-            if operator == '*':
-                sci = mn.scientific(x, y)
-                result = sci.decision(operator)
-                print(x, operator, y, '=', result)
-
-            if operator == '/':
-                sci = mn.scientific(x, y)
-                result = sci.decision(operator)
-                print(x, operator, y, '=', result)
-
-            if operator == '^':
-                sci = mn.scientific(x, y)
-                result = sci.decision(operator)
-                print(x, operator, y, '=', result)
-
-            if str_operator == 'cos':
-                y = float(num[0])
-                sci = mn.new_scientific(y)
-                result = sci.str_decision(str_operator)
-                print(str_operator, y, '=', result)
-
-            if str_operator == 'sin':
-                y = float(num[0])
-                sci = mn.new_scientific(y)
-                result = sci.str_decision(str_operator)
-                print(str_operator, y, '=', result)
-
-            if str_operator == 'tan':
-                y = float(num[0])
-                sci = mn.new_scientific(y)
-                result = sci.str_decision(str_operator)
-                print(str_operator, y, '=', result)
-
-            if str_operator == 'log':
-                y = float(num[0])
-                sci = mn.new_scientific(y)
-                result = sci.str_decision(str_operator)
-                print(str_operator, y, '=', result)
-
-            if operator == '!':
-                str_operator = operator
-                y = int(num[0])
-                sci = mn.new_scientific(y)
-                result = sci.str_decision(str_operator)
-                print(str_operator, y, '=', result)
-
-            # check if user wants another calculation
-            # break the while loop if answer is no
             print('If you want to quit: Write(q)')
-            next_step = input('Next Step: ')
-            if next_step == 'q':
+            calculation = input("Enter your equation: ")
+            if calculation == 'q':
                 break
-            else:
-                for i in next_step:
-                    if i == '+' or i == '-' or i == '*' or i == '/':
-                        operator = i.strip()
 
-                x = result
-                num = re.findall(r'-?\d+\.?\d*', next_step)
-                y = float(num[0])
+            for i in calculation:
+                if i == '+' or i == '-' or i == '*' or i == '/' or i == '^' or i == '!':
+                    operator = i.strip()
+
+            str_operator = ''.join(re.split("[^a-zA-Z]*", calculation))
+            num = re.findall(r'-?\d+\.?\d*', calculation)
+
+            if str_operator == 'sin' or str_operator == 'cos' or str_operator == 'tan' \
+                    or str_operator == 'log' or str_operator == '^':
+
+                x = float(num[0])
+                if str_operator == 'cos':
+                    sci = mn.new_scientific(x)
+                    result = sci.str_decision(str_operator)
+                    print(str_operator, x, '=', result)
+
+                if str_operator == 'sin':
+                    sci = mn.new_scientific(x)
+                    result = sci.str_decision(str_operator)
+                    print(str_operator, x, '=', result)
+
+                if str_operator == 'tan':
+                    sci = mn.new_scientific(x)
+                    result = sci.str_decision(str_operator)
+                    print(str_operator, x, '=', result)
+
+                if str_operator == 'log':
+                    sci = mn.new_scientific(x)
+                    result = sci.str_decision(str_operator)
+                    print(str_operator, x, '=', result)
+
+                quite = input('Do you want to quit this calculator? (y/n): ')
+                if quite == 'y':
+                    break
+                elif quite == 'n':
+                    continue
+                else:
+                    print("Invalid Input")
+
+            if operator == '+' or operator == '-' or operator == '*' or operator == '/' or operator == '^':
+
+                x = float(num[0])
+                y = float(num[1])
+
+                while True:
+                    if operator == '+':
+                        sci = mn.scientific(x, y)
+                        result = sci.decision(operator)
+                        print(x, operator, y, '=', result)
+
+                    if operator == '-':
+                        sci = mn.scientific(x, y)
+                        result = sci.decision(operator)
+                        print(x, operator, y, '=', result)
+
+                    if operator == '*':
+                        sci = mn.scientific(x, y)
+                        result = sci.decision(operator)
+                        print(x, operator, y, '=', result)
+
+                    if operator == '/':
+                        sci = mn.scientific(x, y)
+                        result = sci.decision(operator)
+                        print(x, operator, y, '=', result)
+
+                    if operator == '^':
+                        sci = mn.scientific(x, y)
+                        result = sci.decision(operator)
+                        print(x, operator, y, '=', result)
+
+                    # check if user wants another calculation
+                    # break the while loop if answer is no
+                    print('If you want to quit: Write(q)')
+                    next_step = input('Next Step: ')
+                    if next_step == 'q':
+                        break
+                    else:
+                        for f in next_step:
+                            if f == '+' or f == '-' or f == '*' or f == '/':
+                                operator = f.strip()
+                        x = result
+                        num = re.findall(r'-?\d+\.?\d*', next_step)
+                        y = float(num[0])
 
     if choice == 'q':
         break
